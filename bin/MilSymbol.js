eed0309fa068b88fc808517a3dec9714b16f27c4
Måns Beckman <me@mansbeckman.com>
2014-10-29 10:31:34 +0100

    Added homepage url to license

--- bin/MilSymbol.js
+++ bin/MilSymbol.js
@@ -1,7 +1,7 @@
 ﻿
 /* =======================================================================================
 
-Copyright (c) 2013 Måns Beckman
+Copyright (c) 2013 Måns Beckman  http://www.spatialillusions.com
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
