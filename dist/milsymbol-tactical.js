7bf05bb4f93e67d8dc25a849771c1e5a84cb0354
Måns Beckman <mans.beckman@spatialillusions.com>
2016-12-12 21:32:37 +0100

    Adding Label over ride functionality

--- dist/milsymbol-tactical.js
+++ dist/milsymbol-tactical.js
@@ -57,9 +57,9 @@ function (iconParts, properties, colors, STD2525, monoColor, alternateMedal){
 	alternateMedal
 	true/false for sea mine stuff
 	*/
-	iconParts['TP.DESTROY'] = [{type:'path',fill:false,d:'m 0,45 85,46.75 m 30,16.5 85,46.75 M 0,155 85,108.25 M 115,91.75 200,45'},{type:'text',stroke:false,textanchor:"middle",x:100,y:110,fontsize:32,text:'D'}];
-	iconParts['TP.INTERDICT'] = [{type:'path',fill:false,d:'m 194.203,65.6674 5.49,-20.4904 -20.49,-5.4904 M 115,91.75 200,45 M 0,155 85,108.25 M 185,85 l 15,15 -15,15 m -70,-15 85,0 m -200,0 85,0'},{type:'text',stroke:false,textanchor:"middle",x:100,y:110,fontsize:32,text:'I'}];
-	iconParts['TP.NEUTRALIZE'] = [{type:'path',fill:false,d:'M 115,108.25 200,155 M 0,45 85,91.75'},{type:'path',fill:false,strokedasharray:'12,5',d:'M 115,91.75 200,45 M 0,155 85,108.25'},{type:'text',stroke:false,textanchor:"middle",x:100,y:110,fontsize:32,text:'N'}];
+	iconParts['TP.DESTROY'] = [{type:'path',fill:false,d:'m 0,45 85,46.75 m 30,16.5 85,46.75 M 0,155 85,108.25 M 115,91.75 200,45'},{type:'text',stroke:false,textanchor:"middle",x:100,y:115,fontsize:45,text:'D'}];
+	iconParts['TP.INTERDICT'] = [{type:'path',fill:false,d:'m 194.203,65.6674 5.49,-20.4904 -20.49,-5.4904 M 115,91.75 200,45 M 0,155 85,108.25 M 185,85 l 15,15 -15,15 m -70,-15 85,0 m -200,0 85,0'},{type:'text',stroke:false,textanchor:"middle",x:100,y:115,fontsize:45,text:'I'}];
+	iconParts['TP.NEUTRALIZE'] = [{type:'path',fill:false,d:'M 115,108.25 200,155 M 0,45 85,91.75'},{type:'path',fill:false,strokedasharray:'12,5',d:'M 115,91.75 200,45 M 0,155 85,108.25'},{type:'text',stroke:false,textanchor:"middle",x:100,y:115,fontsize:45,text:'N'}];
 
 
 	iconParts['TP.SONOBUOY'] = [{type:'path',fill:false,d:'M 100,60 l 0,-35 10,10 0,-45'},{type:'circle',fill:false,cx:100,cy:100,r:40}];
@@ -142,6 +142,9 @@ function (iconParts, properties, colors, STD2525, monoColor, alternateMedal){
 	iconParts['TP.UUV - ASW CONTROL STATION'] = [iconParts['TP.(UUV)'],{type:'text',stroke:false,textanchor:"middle",x:100,y:95,fontsize:40,text:'ASW'}];
 	iconParts['TP.UUV - SUW CONTROL STATION'] = [iconParts['TP.(UUV)'],{type:'text',stroke:false,textanchor:"middle",x:100,y:95,fontsize:40,text:'SUW'}];
 	iconParts['TP.UUV - MIW CONTROL STATION'] = [iconParts['TP.(UUV)'],{type:'text',stroke:false,textanchor:"middle",x:100,y:95,fontsize:40,text:'MIW'}];
+
+
+	iconParts['TP.ACTION POINT'] = [{type:'path',fill:false,d:'m 60,45 80,0 m -40,55 -40,-55 0,-105 80,0 0,105 z'}];
 
 
 
@@ -279,7 +282,8 @@ function tacticalPoints(sidc,bbox,icn,_STD2525){
 	bbox['G-G-GPAM--'] = {x1:60,x2:140,y1:30,y2:170};
 	sidc['G-G-GPAN--'] = icn['TP.ORBIT - RANDOM, CLOSED'];//TACGRP.C2GM.GNL.PNT.ACTL.ORBRD
 	bbox['G-G-GPAN--'] = {x1:60,x2:140,y1:30,y2:170};
-	sidc['G-G-GPP---'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT
+	sidc['G-G-GPP---'] = icn['TP.ACTION POINT'];//TACGRP.C2GM.GNL.PNT.ACTPNT
+	bbox['G-G-GPP---'] = {x1:60,x2:140,y1:-60};
 	sidc['G-G-GPPK--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CHKPNT
 	sidc['G-G-GPPC--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CONPNT
 	sidc['G-G-GPPO--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CRDPNT
@@ -441,5 +445,233 @@ function tacticalPoints(sidc,bbox,icn,_STD2525){
 	sidc['G-O-FA----'] = [];//TACGRP.OTH.FIX.ACU
 	sidc['G-O-FE----'] = [];//TACGRP.OTH.FIX.EM
 	sidc['G-O-FO----'] = [];//TACGRP.OTH.FIX.EOP
+}
+);
+
+MS.addLetterLabelOverrides(
+function tacticalPoints(sidc){
+	// Tactical Point Symbols =========================================================================
+	//sidc['G-T-D-----'] = [];//TACGRP.TSK.DSTY
+	//sidc['G-T-I-----'] = [];//TACGRP.TSK.ITDT
+	//sidc['G-T-N-----'] = [];//TACGRP.TSK.NEUT
+	//sidc['G-G-GPUUD-'] = [];//TACGRP.C2GM.GNL.PNT.USW.UH2.DTM
+	//sidc['G-G-GPUUB-'] = [];//TACGRP.C2GM.GNL.PNT.USW.UH2.BCON
+	//sidc['G-G-GPUUL-'] = [];//TACGRP.C2GM.GNL.PNT.USW.UH2.LCON
+	//sidc['G-G-GPUUS-'] = [];//TACGRP.C2GM.GNL.PNT.USW.UH2.SNK
+	//sidc['G-G-GPUY--'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY
+	//sidc['G-G-GPUYP-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.PTNCTR
+	//sidc['G-G-GPUYD-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.DIFAR
+	//sidc['G-G-GPUYL-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.LOFAR
+	//sidc['G-G-GPUYC-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.CASS
+	//sidc['G-G-GPUYS-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.DICASS
+	//sidc['G-G-GPUYB-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.BT
+	//sidc['G-G-GPUYA-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.ANM
+	//sidc['G-G-GPUYV-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.VLAD
+	//sidc['G-G-GPUYT-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.ATAC
+	//sidc['G-G-GPUYR-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.RO
+	//sidc['G-G-GPUYK-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.KGP
+	//sidc['G-G-GPUYX-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SNBY.EXP
+	//sidc['G-G-GPUS--'] = [];//TACGRP.C2GM.GNL.PNT.USW.SRH
+	//sidc['G-G-GPUSA-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SRH.ARA
+	//sidc['G-G-GPUSD-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SRH.DIPPSN
+	//sidc['G-G-GPUSC-'] = [];//TACGRP.C2GM.GNL.PNT.USW.SRH.CTR
+	//sidc['G-G-GPR---'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT
+	//sidc['G-G-GPRN--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.NAVREF
+	//sidc['G-G-GPRS--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.SPLPNT
+	//sidc['G-G-GPRD--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.DLRP
+	//sidc['G-G-GPRP--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.PIM
+	//sidc['G-G-GPRM--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.MRSH
+	//sidc['G-G-GPRW--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.WAP
+	//sidc['G-G-GPRC--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.CRDRTB
+	//sidc['G-G-GPRI--'] = [];//TACGRP.C2GM.GNL.PNT.REFPNT.PNTINR
+	//sidc['G-G-GPWA--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.AIMPNT
+	//sidc['G-G-GPWD--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.DRPPNT
+	//sidc['G-G-GPWE--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.ENTPNT
+	//sidc['G-G-GPWG--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.GRDZRO
+	//sidc['G-G-GPWM--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.MSLPNT
+	//sidc['G-G-GPWI--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.IMTPNT
+	//sidc['G-G-GPWP--'] = [];//TACGRP.C2GM.GNL.PNT.WPN.PIPNT
+	//sidc['G-G-GPF---'] = [];//TACGRP.C2GM.GNL.PNT.FRMN
+	sidc['G-G-GPH---'] = {additionalInformation:{stroke:false,textanchor:"middle",x:100,y:115,fontsize:45,fontweight:'bold'}};//TACGRP.C2GM.GNL.PNT.HBR
+	//sidc['G-G-GPHQ--'] = [];//TACGRP.C2GM.GNL.PNT.HBR.PNTQ
+	//sidc['G-G-GPHA--'] = [];//TACGRP.C2GM.GNL.PNT.HBR.PNTA
+	//sidc['G-G-GPHY--'] = [];//TACGRP.C2GM.GNL.PNT.HBR.PNTY
+	//sidc['G-G-GPHX--'] = [];//TACGRP.C2GM.GNL.PNT.HBR.PNTX
+	//sidc['G-G-GPO---'] = [];//TACGRP.C2GM.GNL.PNT.RTE
+	//sidc['G-G-GPOZ--'] = [];//TACGRP.C2GM.GNL.PNT.RTE.RDV
+	//sidc['G-G-GPOD--'] = [];//TACGRP.C2GM.GNL.PNT.RTE.DVSN
+	//sidc['G-G-GPOW--'] = [];//TACGRP.C2GM.GNL.PNT.RTE.WAP
+	//sidc['G-G-GPOP--'] = [];//TACGRP.C2GM.GNL.PNT.RTE.PIM
+	//sidc['G-G-GPOR--'] = [];//TACGRP.C2GM.GNL.PNT.RTE.PNTR
+	//sidc['G-G-GPA---'] = [];//TACGRP.C2GM.GNL.PNT.ACTL
+	//sidc['G-G-GPAP--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.CAP
+	//sidc['G-G-GPAW--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ABNEW
+	//sidc['G-G-GPAK--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.TAK
+	//sidc['G-G-GPAA--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ASBWF
+	//sidc['G-G-GPAH--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ASBWR
+	//sidc['G-G-GPAB--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.SUWF
+	//sidc['G-G-GPAC--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.SUWR
+	//sidc['G-G-GPAD--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.MIWF
+	//sidc['G-G-GPAE--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.MIWR
+	//sidc['G-G-GPAS--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.SKEIP
+	//sidc['G-G-GPAT--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.TCN
+	//sidc['G-G-GPAO--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.TMC
+	//sidc['G-G-GPAR--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.RSC
+	//sidc['G-G-GPAL--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.RPH
+	//sidc['G-G-GPAF--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.UA
+	//sidc['G-G-GPAG--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.VTUA
+	//sidc['G-G-GPAI--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ORB
+	//sidc['G-G-GPAJ--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ORBF8
+	//sidc['G-G-GPAM--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ORBRT
+	//sidc['G-G-GPAN--'] = [];//TACGRP.C2GM.GNL.PNT.ACTL.ORBRD
+	sidc['G-G-GPP---'] = {	additionalInformation:{stroke:false,textanchor:"middle",x:100,y:-70,fontsize:40},
+							additionalInformation1:{stroke:false,textanchor:"middle",x:100,y:-20,fontsize:35,fontweight:'bold'},
+							hostile:{stroke:false,textanchor:"start",x:150,y:50,fontsize:40},
+							uniqueDesignation:{stroke:false,textanchor:"start",x:150,y:0,fontsize:80},
+							dtg:{stroke:false,textanchor:"end",x:50,y:-30,fontsize:40},
+							dtg1:{stroke:false,textanchor:"end",x:50,y:10,fontsize:40}};//TACGRP.C2GM.GNL.PNT.ACTPNT
+	//sidc['G-G-GPPK--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CHKPNT
+	//sidc['G-G-GPPC--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CONPNT
+	//sidc['G-G-GPPO--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.CRDPNT
+	//sidc['G-G-GPPD--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.DCNPNT
+	//sidc['G-G-GPPL--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.LNKUPT
+	//sidc['G-G-GPPP--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.PSSPNT
+	//sidc['G-G-GPPR--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.RAYPNT
+	//sidc['G-G-GPPE--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.RELPNT
+	//sidc['G-G-GPPS--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.STRPNT
+	//sidc['G-G-GPPA--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.AMNPNT
+	//sidc['G-G-GPPW--'] = [];//TACGRP.C2GM.GNL.PNT.ACTPNT.WAP
+	//sidc['G-G-GPC---'] = [];//TACGRP.C2GM.GNL.PNT.SCTL
+	//sidc['G-G-GPCU--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.USV
+	//sidc['G-G-GPCUR-'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.USV.RMV
+	//sidc['G-G-GPCUA-'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.USV.ASW
+	//sidc['G-G-GPCUS-'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.USV.SUW
+	//sidc['G-G-GPCUM-'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.USV.MIW
+	//sidc['G-G-GPCA--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.ASW
+	//sidc['G-G-GPCS--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.SUW
+	//sidc['G-G-GPCM--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.MIW
+	//sidc['G-G-GPCP--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.PKT
+	//sidc['G-G-GPCR--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.RDV
+	//sidc['G-G-GPCC--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.RSC
+	//sidc['G-G-GPCE--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.REP
+	//sidc['G-G-GPCN--'] = [];//TACGRP.C2GM.GNL.PNT.SCTL.NCBTT
+	//sidc['G-G-GPB---'] = [];//TACGRP.C2GM.GNL.PNT.UCTL
+	//sidc['G-G-GPBU--'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.UUV
+	//sidc['G-G-GPBUA-'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.UUV.ASW
+	//sidc['G-G-GPBUS-'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.UUV.SUW
+	//sidc['G-G-GPBUM-'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.UUV.MIW
+	//sidc['G-G-GPBS--'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.SBSTN
+	//sidc['G-G-GPBSA-'] = [];//TACGRP.C2GM.GNL.PNT.UCTL.SBSTN.ASW
+	//sidc['G-G-APP---'] = [];//TACGRP.C2GM.AVN.PNT.ACP
+	//sidc['G-G-APC---'] = [];//TACGRP.C2GM.AVN.PNT.COMMCP
+	//sidc['G-G-APU---'] = [];//TACGRP.C2GM.AVN.PNT.PUP
+	//sidc['G-G-APD---'] = [];//TACGRP.C2GM.AVN.PNT.DAPP
+	//sidc['G-G-PN----'] = [];//TACGRP.C2GM.DCPN.DMYMS
+	//sidc['G-G-DPT---'] = [];//TACGRP.C2GM.DEF.PNT.TGTREF
+	//sidc['G-G-DPO---'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST
+	//sidc['G-G-DPOC--'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST.CBTPST
+	//sidc['G-G-DPOR--'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST.RECON
+	//sidc['G-G-DPOF--'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST.FWDOP
+	//sidc['G-G-DPOS--'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST.SOP
+	//sidc['G-G-DPON--'] = [];//TACGRP.C2GM.DEF.PNT.OBSPST.CBRNOP
+	//sidc['G-G-OPP---'] = [];//TACGRP.C2GM.OFF.PNT.PNTD
+	//sidc['G-M-OAOF--'] = [];//TACGRP.MOBSU.OBST.ATO.TDTSM.FIXPFD
+	//sidc['G-M-OAOM--'] = [];//TACGRP.MOBSU.OBST.ATO.TDTSM.MVB
+	//sidc['G-M-OAOP--'] = [];//TACGRP.MOBSU.OBST.ATO.TDTSM.MVBPFD
+	//sidc['G-M-OB----'] = [];//TACGRP.MOBSU.OBST.BBY
+	//sidc['G-M-OMU---'] = [];//TACGRP.MOBSU.OBST.MNE.USPMNE
+	//sidc['G-M-OMT---'] = [];//TACGRP.MOBSU.OBST.MNE.ATMNE
+	//sidc['G-M-OMD---'] = [];//TACGRP.MOBSU.OBST.MNE.ATMAHD
+	//sidc['G-M-OME---'] = [];//TACGRP.MOBSU.OBST.MNE.ATMDIR
+	//sidc['G-M-OMP---'] = [];//TACGRP.MOBSU.OBST.MNE.APMNE
+	//sidc['G-M-OMW---'] = [];//TACGRP.MOBSU.OBST.MNE.WAMNE
+	//sidc['G-M-OFS---'] = [];//TACGRP.MOBSU.OBST.MNEFLD.STC
+	//sidc['G-M-OHTL--'] = [];//TACGRP.MOBSU.OBST.AVN.TWR.LOW
+	//sidc['G-M-OHTH--'] = [];//TACGRP.MOBSU.OBST.AVN.TWR.HIGH
+	//sidc['G-M-BCP---'] = [];//TACGRP.MOBSU.OBSTBP.CSGSTE.ERP
+	//sidc['G-M-SE----'] = [];//TACGRP.MOBSU.SU.ESTOF
+	//sidc['G-M-SF----'] = [];//TACGRP.MOBSU.SU.FRT
+	//sidc['G-M-SS----'] = [];//TACGRP.MOBSU.SU.SUFSHL
+	//sidc['G-M-SU----'] = [];//TACGRP.MOBSU.SU.UGDSHL
+	//sidc['G-M-NZ----'] = [];//TACGRP.MOBSU.CBRN.NDGZ
+	//sidc['G-M-NF----'] = [];//TACGRP.MOBSU.CBRN.FAOTP
+	//sidc['G-M-NEB---'] = [];//TACGRP.MOBSU.CBRN.REEVNT.BIO
+	//sidc['G-M-NEC---'] = [];//TACGRP.MOBSU.CBRN.REEVNT.CML
+	//sidc['G-M-NDP---'] = [];//TACGRP.MOBSU.CBRN.DECONP.USP
+	//sidc['G-M-NDA---'] = [];//TACGRP.MOBSU.CBRN.DECONP.ALTUSP
+	//sidc['G-M-NDT---'] = [];//TACGRP.MOBSU.CBRN.DECONP.TRP
+	//sidc['G-M-NDE---'] = [];//TACGRP.MOBSU.CBRN.DECONP.EQT
+	//sidc['G-M-NDB---'] = [];//TACGRP.MOBSU.CBRN.DECONP.EQTTRP
+	//sidc['G-M-NDO---'] = [];//TACGRP.MOBSU.CBRN.DECONP.OPDECN
+	//sidc['G-M-NDD---'] = [];//TACGRP.MOBSU.CBRN.DECONP.TRGH
+	//sidc['G-F-PTS---'] = [];//TACGRP.FSUPP.PNT.TGT.PTGT
+	//sidc['G-F-PTN---'] = [];//TACGRP.FSUPP.PNT.TGT.NUCTGT
+	//sidc['G-F-PCF---'] = [];//TACGRP.FSUPP.PNT.C2PNT.FSS
+	//sidc['G-F-PCS---'] = [];//TACGRP.FSUPP.PNT.C2PNT.SCP
+	//sidc['G-F-PCB---'] = [];//TACGRP.FSUPP.PNT.C2PNT.FP
+	//sidc['G-F-PCR---'] = [];//TACGRP.FSUPP.PNT.C2PNT.RP
+	//sidc['G-F-PCH---'] = [];//TACGRP.FSUPP.PNT.C2PNT.HP
+	//sidc['G-F-PCL---'] = [];//TACGRP.FSUPP.PNT.C2PNT.LP
+	//sidc['G-F-ATR---'] = [];//TACGRP.FSUPP.ARS.ARATGT.RTGTGT
+	//sidc['G-F-ATC---'] = [];//TACGRP.FSUPP.ARS.ARATGT.CIRTGT
+	//sidc['G-F-ACSC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.FSA.CIRCLR
+	//sidc['G-F-ACAC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.ACA.CIRCLR
+	//sidc['G-F-ACFC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.FFA.CIRCLR
+	//sidc['G-F-ACNC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.NFA.CIRCLR
+	//sidc['G-F-ACRC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.RFA.CIRCLR
+	//sidc['G-F-ACPC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.PAA.CIRCLR
+	//sidc['G-F-ACEC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.SNSZ.CIRCLR
+	//sidc['G-F-ACDC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.DA.CIRCLR
+	//sidc['G-F-ACZC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.ZOR.CIRCLR
+	//sidc['G-F-ACBC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.TBA.CIRCLR
+	//sidc['G-F-ACVC--'] = [];//TACGRP.FSUPP.ARS.C2ARS.TVAR.CIRCLR
+	//sidc['G-F-AXC---'] = [];//TACGRP.FSUPP.ARS.WPNRF.CIRCLR
+	//sidc['G-F-AKBC--'] = [];//TACGRP.FSUPP.ARS.KLBOX.BLUE.CIRCLR
+	//sidc['G-F-AKPC--'] = [];//TACGRP.FSUPP.ARS.KLBOX.PURPLE.CIRCLR
+	//sidc['G-S-PX----'] = [];//TACGRP.CSS.PNT.AEP
+	//sidc['G-S-PC----'] = [];//TACGRP.CSS.PNT.CBNP
+	//sidc['G-S-PY----'] = [];//TACGRP.CSS.PNT.CCP
+	//sidc['G-S-PT----'] = [];//TACGRP.CSS.PNT.CVP
+	//sidc['G-S-PD----'] = [];//TACGRP.CSS.PNT.DCP
+	//sidc['G-S-PE----'] = [];//TACGRP.CSS.PNT.EPWCP
+	//sidc['G-S-PL----'] = [];//TACGRP.CSS.PNT.LRP
+	//sidc['G-S-PM----'] = [];//TACGRP.CSS.PNT.MCP
+	//sidc['G-S-PR----'] = [];//TACGRP.CSS.PNT.RRRP
+	//sidc['G-S-PU----'] = [];//TACGRP.CSS.PNT.ROM
+	//sidc['G-S-PO----'] = [];//TACGRP.CSS.PNT.TCP
+	//sidc['G-S-PI----'] = [];//TACGRP.CSS.PNT.TTP
+	//sidc['G-S-PN----'] = [];//TACGRP.CSS.PNT.UMC
+	//sidc['G-S-PS----'] = [];//TACGRP.CSS.PNT.SPT
+	//sidc['G-S-PSZ---'] = [];//TACGRP.CSS.PNT.SPT.GNL
+	//sidc['G-S-PSA---'] = [];//TACGRP.CSS.PNT.SPT.CLS1
+	//sidc['G-S-PSB---'] = [];//TACGRP.CSS.PNT.SPT.CLS2
+	//sidc['G-S-PSC---'] = [];//TACGRP.CSS.PNT.SPT.CLS3
+	//sidc['G-S-PSD---'] = [];//TACGRP.CSS.PNT.SPT.CLS4
+	//sidc['G-S-PSE---'] = [];//TACGRP.CSS.PNT.SPT.CLS5
+	//sidc['G-S-PSF---'] = [];//TACGRP.CSS.PNT.SPT.CLS6
+	//sidc['G-S-PSG---'] = [];//TACGRP.CSS.PNT.SPT.CLS7
+	//sidc['G-S-PSH---'] = [];//TACGRP.CSS.PNT.SPT.CLS8
+	//sidc['G-S-PSI---'] = [];//TACGRP.CSS.PNT.SPT.CLS9
+	//sidc['G-S-PSJ---'] = [];//TACGRP.CSS.PNT.SPT.CLS10
+	//sidc['G-S-PA----'] = [];//TACGRP.CSS.PNT.AP
+	//sidc['G-S-PAS---'] = [];//TACGRP.CSS.PNT.AP.ASP
+	//sidc['G-S-PAT---'] = [];//TACGRP.CSS.PNT.AP.ATP
+	//sidc['G-O-ED----'] = [];//TACGRP.OTH.ER.DTHAC
+	//sidc['G-O-EP----'] = [];//TACGRP.OTH.ER.PIW
+	//sidc['G-O-EV----'] = [];//TACGRP.OTH.ER.DSTVES
+	//sidc['G-O-HM----'] = [];//TACGRP.OTH.HAZ.SML
+	//sidc['G-O-HI----'] = [];//TACGRP.OTH.HAZ.IB
+	//sidc['G-O-HO----'] = [];//TACGRP.OTH.HAZ.OLRG
+	//sidc['G-O-SB----'] = [];//TACGRP.OTH.SSUBSR.BTMRTN
+	//sidc['G-O-SBM---'] = [];//TACGRP.OTH.SSUBSR.BTMRTN.INS
+	//sidc['G-O-SBN---'] = [];//TACGRP.OTH.SSUBSR.BTMRTN.SBRSOO
+	//sidc['G-O-SBW---'] = [];//TACGRP.OTH.SSUBSR.BTMRTN.WRKND
+	//sidc['G-O-SBX---'] = [];//TACGRP.OTH.SSUBSR.BTMRTN.WRKD
+	//sidc['G-O-SM----'] = [];//TACGRP.OTH.SSUBSR.MARLFE
+	//sidc['G-O-SS----'] = [];//TACGRP.OTH.SSUBSR.SA
+	//sidc['G-O-FA----'] = [];//TACGRP.OTH.FIX.ACU
+	//sidc['G-O-FE----'] = [];//TACGRP.OTH.FIX.EM
+	//sidc['G-O-FO----'] = [];//TACGRP.OTH.FIX.EOP
+
 }
 );
