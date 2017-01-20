94479e7370dcd630e4961ac6389bb1916cf22d3e
Måns Beckman <mans.beckman@spatialillusions.com>
2017-01-20 23:11:22 +0100

    Update copyright in license to 2017

--- src/ms.js
+++ src/ms.js
@@ -1,6 +1,6 @@
 /*! ======================================================================================
 
-Copyright (c) 2015 Måns Beckman  http://www.spatialillusions.com
+Copyright (c) 2017 Måns Beckman  http://www.spatialillusions.com
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
