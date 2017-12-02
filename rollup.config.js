cbd97c86a4e93fd13d1d128416982b85210626f7
Wolfgang <wolf@okwolf.com>
2017-12-02 11:19:22 -0800

    swapping out webpack for rollup/uglify

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/milsymbol.development.js',
+    format: 'umd'
+  },
+  name: 'ms',
+  plugins: [
+    resolve(),
+    commonjs()
+  ]
+};
