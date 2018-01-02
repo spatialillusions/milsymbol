3016d3f3d7d3c2987d77e15a263aae1e002f0186
Måns Beckman <mans.beckman@spatialillusions.com>
2018-01-02 16:58:55 +0100

    Build documentation from JSON files

--- src/2525d.js
+++ src/2525d.js
@@ -1,19 +1,19 @@
 // Creates a js file with all of 2525 D
-import * as air from "../dev/2525D 01 air.json";
-import * as airmissile from "../dev/2525D 02 air-missile.json";
-import * as space from "../dev/2525D 05 space.json";
-import * as spacemissile from "../dev/2525D 06 space-missile.json";
-import * as landunit from "../dev/2525D 10 land-unit.json";
-import * as landcivilian from "../dev/2525D 11 land-civilian.json";
-import * as landequipment from "../dev/2525D 15 land-equipment.json";
-import * as landinstallation from "../dev/2525D 20 land-installation.json";
-import * as controlmeasure from "../dev/2525D 25 control-measure.json";
-import * as seasurface from "../dev/2525D 30 sea-surface.json";
-import * as seasubsurface from "../dev/2525D 35 sea-subsurface.json";
-import * as minewarfare from "../dev/2525D 36 mine-warfare.json";
-import * as activities from "../dev/2525D 40 activities.json";
-import * as signalsintelligence from "../dev/2525D 50 signals-intelligence.json";
-import * as cyberspace from "../dev/2525D 60 cyberspace.json";
+import air from "../dev/2525D 01 air.json";
+import airmissile from "../dev/2525D 02 air-missile.json";
+import space from "../dev/2525D 05 space.json";
+import spacemissile from "../dev/2525D 06 space-missile.json";
+import landunit from "../dev/2525D 10 land-unit.json";
+import landcivilian from "../dev/2525D 11 land-civilian.json";
+import landequipment from "../dev/2525D 15 land-equipment.json";
+import landinstallation from "../dev/2525D 20 land-installation.json";
+import controlmeasure from "../dev/2525D 25 control-measure.json";
+import seasurface from "../dev/2525D 30 sea-surface.json";
+import seasubsurface from "../dev/2525D 35 sea-subsurface.json";
+import minewarfare from "../dev/2525D 36 mine-warfare.json";
+import activities from "../dev/2525D 40 activities.json";
+import signalsintelligence from "../dev/2525D 50 signals-intelligence.json";
+import cyberspace from "../dev/2525D 60 cyberspace.json";
 export default {
   "01": air,
   "02": airmissile,
--- src/app6d.js
+++ src/app6d.js
@@ -1,17 +1,17 @@
 // Creates a js file with all of APP-6 D
-import * as air from "../dev/APP6-D 01 air.json";
-import * as airmissile from "../dev/APP6-D 02 air-missile.json";
-import * as space from "../dev/APP6-D 05 space.json";
-import * as landunit from "../dev/APP6-D 10 land-unit.json";
-import * as landcivilian from "../dev/APP6-D 11 land-civilian.json";
-import * as landequipment from "../dev/APP6-D 15 land-equipment.json";
-import * as landinstallation from "../dev/APP6-D 20 land-installation.json";
-import * as controlmeasure from "../dev/APP6-D 25 control-measure.json";
-import * as dismountedindividual from "../dev/APP6-D 27 dismounted-individual.json";
-import * as seasurface from "../dev/APP6-D 30 sea-surface.json";
-import * as seasubsurface from "../dev/APP6-D 35 sea-subsurface.json";
-import * as minewarfare from "../dev/APP6-D 36 mine-warfare.json";
-import * as activities from "../dev/APP6-D 40 activities.json";
+import air from "../dev/APP6-D 01 air.json";
+import airmissile from "../dev/APP6-D 02 air-missile.json";
+import space from "../dev/APP6-D 05 space.json";
+import landunit from "../dev/APP6-D 10 land-unit.json";
+import landcivilian from "../dev/APP6-D 11 land-civilian.json";
+import landequipment from "../dev/APP6-D 15 land-equipment.json";
+import landinstallation from "../dev/APP6-D 20 land-installation.json";
+import controlmeasure from "../dev/APP6-D 25 control-measure.json";
+import dismountedindividual from "../dev/APP6-D 27 dismounted-individual.json";
+import seasurface from "../dev/APP6-D 30 sea-surface.json";
+import seasubsurface from "../dev/APP6-D 35 sea-subsurface.json";
+import minewarfare from "../dev/APP6-D 36 mine-warfare.json";
+import activities from "../dev/APP6-D 40 activities.json";
 export default {
   "01": air,
   "02": airmissile,
