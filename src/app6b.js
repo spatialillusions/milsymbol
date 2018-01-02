29921d11b975d570549bf8b14a2ed2721db4cf6f
Måns Beckman <mans.beckman@spatialillusions.com>
2018-01-02 22:56:41 +0100

    Update documentation APP-6 B

--- src/app6b.js
+++ src/app6b.js
@@ -1,5 +1,5 @@
 // Creates a js file with all of APP-6 B
-import warfighting from "../dev/APP6-B.json";
+import warfighting from "../dev/APP6-B warfighting.json";
 import tacticalgraphics from "../dev/APP6-B tactical-graphics.json";
 export default {
   S: warfighting,
