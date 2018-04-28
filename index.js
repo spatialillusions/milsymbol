8acf04bf5c60b37373967c755f5ebd2634a819e7
Måns Beckman <mans.beckman@spatialillusions.com>
2018-04-28 11:46:02 +0200

    Move to root

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
 /* ***************************************************************************************
 Creating the base of milsymbol
 *************************************************************************************** */
-import { ms } from "./ms.js";
-//import { version } from "../package.json";
-//ms.version = version;
+import { ms } from "./src/milsymbol.js";
 
 /* ***************************************************************************************
 Letter based SIDC
 *************************************************************************************** */
-import spaceletter from "./lettersidc/sidc/space.js";
+import spaceletter from "./src/lettersidc/sidc/space.js";
 ms.addIcons(spaceletter);
-import airletter from "./lettersidc/sidc/air.js";
+import airletter from "./src/lettersidc/sidc/air.js";
 ms.addIcons(airletter);
-import groundletter from "./lettersidc/sidc/ground.js";
+import groundletter from "./src/lettersidc/sidc/ground.js";
 ms.addIcons(groundletter);
-import equipmentletter from "./lettersidc/sidc/equipment.js";
+import equipmentletter from "./src/lettersidc/sidc/equipment.js";
 ms.addIcons(equipmentletter);
-import installationsletter from "./lettersidc/sidc/installations.js";
+import installationsletter from "./src/lettersidc/sidc/installations.js";
 ms.addIcons(installationsletter);
-import sealetter from "./lettersidc/sidc/sea.js";
+import sealetter from "./src/lettersidc/sidc/sea.js";
 ms.addIcons(sealetter);
-import subsurfaceletter from "./lettersidc/sidc/subsurface.js";
+import subsurfaceletter from "./src/lettersidc/sidc/subsurface.js";
 ms.addIcons(subsurfaceletter);
-import sofletter from "./lettersidc/sidc/sof.js";
+import sofletter from "./src/lettersidc/sidc/sof.js";
 ms.addIcons(sofletter);
-import signalsIntelligenceletter from "./lettersidc/sidc/signalsIntelligence.js";
+import signalsIntelligenceletter from "./src/lettersidc/sidc/signalsIntelligence.js";
 ms.addIcons(signalsIntelligenceletter);
-import stabilityoperationsletter from "./lettersidc/sidc/stabilityoperations.js";
+import stabilityoperationsletter from "./src/lettersidc/sidc/stabilityoperations.js";
 ms.addIcons(stabilityoperationsletter);
-import emergencymanagementsymbolsletter from "./lettersidc/sidc/emergencymanagementsymbols.js";
+import emergencymanagementsymbolsletter from "./src/lettersidc/sidc/emergencymanagementsymbols.js";
 ms.addIcons(emergencymanagementsymbolsletter);
 // Adding support for symbols in MIL-STD-2525B change 2 that are missing in 2525C
-import std2525bletter from "./lettersidc/sidc/2525b-ch2.js";
+import std2525bletter from "./src/lettersidc/sidc/2525b-ch2.js";
 ms.addIcons(std2525bletter);
 // Adding support for tactical points
-import tacticalpoints2525letter from "./lettersidc/sidc/tactical-points-2525.js";
+import tacticalpoints2525letter from "./src/lettersidc/sidc/tactical-points-2525.js";
 ms.addIcons(tacticalpoints2525letter);
-import tacticalpointsapp6letter from "./lettersidc/sidc/tactical-points-app6.js";
+import tacticalpointsapp6letter from "./src/lettersidc/sidc/tactical-points-app6.js";
 ms.addIcons(tacticalpointsapp6letter);
 
 /* ***************************************************************************************
 Number based SIDC
 *************************************************************************************** */
-import airnumber from "./numbersidc/sidc/air.js";
+import airnumber from "./src/numbersidc/sidc/air.js";
 ms.addIcons(airnumber);
-import airmissilenumber from "./numbersidc/sidc/airmissile.js";
+import airmissilenumber from "./src/numbersidc/sidc/airmissile.js";
 ms.addIcons(airmissilenumber);
-import spacenumber from "./numbersidc/sidc/space.js";
+import spacenumber from "./src/numbersidc/sidc/space.js";
 ms.addIcons(spacenumber);
-import spacemissilenumber from "./numbersidc/sidc/spacemissile.js";
+import spacemissilenumber from "./src/numbersidc/sidc/spacemissile.js";
 ms.addIcons(spacemissilenumber);
-import landunitnumber from "./numbersidc/sidc/landunit.js";
+import landunitnumber from "./src/numbersidc/sidc/landunit.js";
 ms.addIcons(landunitnumber);
-import landciviliannumber from "./numbersidc/sidc/landcivilian.js";
+import landciviliannumber from "./src/numbersidc/sidc/landcivilian.js";
 ms.addIcons(landciviliannumber);
-import landequipmentnumber from "./numbersidc/sidc/landequipment.js";
+import landequipmentnumber from "./src/numbersidc/sidc/landequipment.js";
 ms.addIcons(landequipmentnumber);
-import landinstallationnumber from "./numbersidc/sidc/landinstallation.js";
+import landinstallationnumber from "./src/numbersidc/sidc/landinstallation.js";
 ms.addIcons(landinstallationnumber);
-import seanumber from "./numbersidc/sidc/sea.js";
+import seanumber from "./src/numbersidc/sidc/sea.js";
 ms.addIcons(seanumber);
-import subsurfacenumber from "./numbersidc/sidc/subsurface.js";
+import subsurfacenumber from "./src/numbersidc/sidc/subsurface.js";
 ms.addIcons(subsurfacenumber);
-import minewarfarenumber from "./numbersidc/sidc/minewarfare.js";
+import minewarfarenumber from "./src/numbersidc/sidc/minewarfare.js";
 ms.addIcons(minewarfarenumber);
-import activitesnumber from "./numbersidc/sidc/activites.js";
+import activitesnumber from "./src/numbersidc/sidc/activites.js";
 ms.addIcons(activitesnumber);
-import signalsintelligencenumber from "./numbersidc/sidc/signalsintelligence.js";
+import signalsintelligencenumber from "./src/numbersidc/sidc/signalsintelligence.js";
 ms.addIcons(signalsintelligencenumber);
-import cyberspacenumber from "./numbersidc/sidc/cyberspace.js";
+import cyberspacenumber from "./src/numbersidc/sidc/cyberspace.js";
 ms.addIcons(cyberspacenumber);
-import dismountedindividualnumber from "./numbersidc/sidc/dismountedindividual.js";
+import dismountedindividualnumber from "./src/numbersidc/sidc/dismountedindividual.js";
 ms.addIcons(dismountedindividualnumber);
-import tacticalpointsnumber from "./numbersidc/sidc/control-measure.js";
+import tacticalpointsnumber from "./src/numbersidc/sidc/control-measure.js";
 ms.addIcons(tacticalpointsnumber);
 
 /* ***************************************************************************************
@@ -84,7 +82,7 @@ This draws the symbol octagon, can be good for debugging.
 /* ***************************************************************************************
 This makes it possible to draw canvas paths in IE11
 *************************************************************************************** */
-import path2d from "./ms/path2d.js";
+import path2d from "./src/ms/path2d.js";
 ms._Path2D = path2d;
 
 /* ***************************************************************************************
