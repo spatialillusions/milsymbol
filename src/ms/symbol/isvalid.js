e050f4e1b2138de68ee1eaf6b71ea8285b2e1546
Måns Beckman <mans.beckman@spatialillusions.com>
2018-10-25 17:54:27 +0200

    Add check for affiliation and dimension to validation control
    #203

--- src/ms/symbol/isvalid.js
+++ src/ms/symbol/isvalid.js
@@ -4,13 +4,23 @@ export default function isValid(extended) {
 
   if (extended) {
     return {
+      affiliation: this.metadata.affiliation,
+      dimension: this.metadata.dimension,
+      dimensionUnknown: this.metadata.dimensionUnknown,
       drawInstructions: drawInstructions,
       icon: this.validIcon,
       mobility: this.metadata.mobility != undefined
     };
   } else {
     return (
-      drawInstructions && this.validIcon && this.metadata.mobility != undefined
+      !(
+        this.metadata.affiliation == "undefined" ||
+        (this.metadata.dimension == "undefined" &&
+          !this.metadata.controlMeasure)
+      ) &&
+      drawInstructions &&
+      this.validIcon &&
+      this.metadata.mobility != undefined
     );
   }
 }
