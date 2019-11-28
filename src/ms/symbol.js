9fb0207fa8ac143db507c1a471d72065f7bddcee
Yevhenii <freewindsua@gmail.com>
2019-11-28 08:37:18 +0200

    Update symbol.js

--- src/ms/symbol.js
+++ src/ms/symbol.js
@@ -122,7 +122,7 @@ import setOptions from "./symbol/setoptions.js";
 Symbol.prototype.setOptions = setOptions;
 
 Symbol.prototype.toDataURL = function() {
-      "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(this.asSVG())))
+      return "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(this.asSVG())));
 };
 
 export default Symbol;
