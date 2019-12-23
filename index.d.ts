e48a55f5187949327401d88d130203f30f5e8b1f
Rémi <r3mi@users.sourceforge.net>
2019-12-23 18:39:25 +0100

    fix typescript typings for constructor to allow any number of arguments

--- index.d.ts
+++ index.d.ts
@@ -108,8 +108,7 @@ type SymbolColors = {
 export class Symbol {
   constructor(
     code: string | SymbolOptions,
-    opts?: SymbolOptions,
-    style?: SymbolOptions
+    ...options: SymbolOptions[]
   );
 
   asCanvas(factor?: number): HTMLCanvasElement;
