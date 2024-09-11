73076d39b90420c4efd05990020f488a28b69aa2
Måns Beckman <mans.beckman@spatialillusions.com>
2024-09-11 07:36:38 +0200

    Add cyberspace modifiers

--- src/numbersidc/sidc/cyberspace.js
+++ src/numbersidc/sidc/cyberspace.js
@@ -54,8 +54,41 @@ export default {
       sId["170100"] = [icn["CY.IC.SERVER"]];
       sId["170200"] = [icn["CY.IC.WORKSTATION"]];
       sId["170300"] = [];
-
+      sId["170301"] = [icn["CY.IC.MOBILE"]];
+      sId["170302"] = [icn["CY.IC.TABLET"]];
+      sId["170303"] = [icn["CY.IC.LAPTOP"]];
+      sId["170304"] = [icn["CY.IC.HEALTH MONITOR"]];
+      sId["170305"] = [icn["CY.IC.SMARTVEST"]];
+      sId["170306"] = [icn["CY.IC.SMARTWATCH"]];
+      sId["170307"] = [icn["CY.IC.WEARABLE"]];
+      sId["170400"] = [icn["CY.IC.IOT"]];
+      sId["170500"] = [icn["CY.IC.PRINTER"]];
+      sId["170600"] = [icn["CY.IC.ROUTER"]];
+      sId["170700"] = [icn["CY.IC.SWITCH"]];
       sId["180000"] = [icn["CY.IC.NETWORK"]];
+
+      sIdm1["01"] = [icn["CY.M1.DEFENSIVE CYBERSPACE"]];
+      sIdm1["02"] = [icn["CY.M1.OFFENSIVE CYBERSPACE"]];
+      sIdm1["03"] = [icn["CY.M1.SOCIAL"]];
+      sIdm1["04"] = [icn["CY.M1.WIRED"]];
+      sIdm1["05"] = [icn["CY.M1.WIRELESS"]];
+      sIdm1["06"] = [icn["CY.M1.RADIO FREQUENCY"]];
+      sIdm1["07"] = [icn["CY.M1.OPERATING SYSTEM"]];
+      sIdm1["08"] = [icn["CY.M1.CONTINUITY OF OPERATIONS"]];
+      sIdm1["09"] = [icn["CY.M1.INTERNET SERVICE PROVIDER"]];
+      sIdm1["10"] = [icn["CY.M1.FINANCE"]];
+      sIdm1["11"] = [icn["CY.M1.OUT OF BAND"]];
+      sIdm1["12"] = [icn["CY.M1.CLOUD"]];
+      sIdm1["13"] = [icn["CY.M1.DATA"]];
+
+      sIdm2["01"] = [icn["CY.M2.SECURED"]];
+      sIdm2["02"] = [icn["CY.M2.OPEN"]];
+      sIdm2["03"] = [icn["CY.M2.RESPONSE ACTIONS"]];
+      //sIdm2["04"] = [];
+      sIdm2["05"] = [icn["CY.M2.INTERNAL DEFENSE MEASURES"]];
+      sIdm2["06"] = [icn["CY.M2.ADVANCED PERSISTENT THREAT"]];
+      sIdm2["07"] = [icn["CY.M2.NATION STATE"]];
+      sIdm2["08"] = [icn["CY.M2.NATION STATE SPONSORED"]];
     }
   }
 };
