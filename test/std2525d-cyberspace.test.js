93154733a793d00296affa8d1086da298f8cfb77
Måns Beckman <mans.beckman@spatialillusions.com>
2024-10-11 15:09:22 +0200

    Use 2525E since there are big changes since 2525D

--- test/std2525d-cyberspace.test.js
+++ test/std2525d-cyberspace.test.js
@@ -1,9 +1,9 @@
 import { ms } from "../src/milsymbol";
 ms.reset();
-import { ms2525d } from "mil-std-2525";
+import { ms2525e } from "milstandard-e";
 import verify from "./number-sidc";
 
 import { cyberspace as icons } from "../src/numbersidc";
 ms.addIcons(icons);
 
-export default verify(ms, ms2525d["60"]);
+export default verify(ms, ms2525e["60"]);
