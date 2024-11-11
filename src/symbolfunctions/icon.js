bfda52e999762ae7d587e38853af882d01a170c5
Måns Beckman <mans.beckman@spatialillusions.com>
2024-11-11 21:22:06 +0100

    Do not scale frame swapped symbols

--- src/symbolfunctions/icon.js
+++ src/symbolfunctions/icon.js
@@ -139,7 +139,7 @@ export default function icon(ms) {
     //Number based SIDCs.
     if (this.metadata.numberSIDC) {
       //Number based SIDCs.
-      const symbolSet = String(this.options.sidc).substr(4, 2);
+      const symbolSet = String(this.options.sidc).substring(4, 6);
       if (ms._iconCache[icnet].hasOwnProperty("numberSIDC")) {
         if (
           ms._iconCache[icnet].numberSIDC.symbolSet.hasOwnProperty(symbolSet)
@@ -151,14 +151,13 @@ export default function icon(ms) {
             ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].bbox;
         } else {
           if (typeof ms._getIcons.number === "function") {
-            ms._iconCache[icnet].numberSIDC.symbolSet[
-              symbolSet
-            ] = ms._getIcons.number(
-              ms,
-              symbolSet,
-              iconParts,
-              this.metadata.STD2525
-            );
+            ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet] =
+              ms._getIcons.number(
+                ms,
+                symbolSet,
+                iconParts,
+                this.metadata.STD2525
+              );
             icons = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].icons;
             m1 = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].m1;
             m2 = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].m2;
@@ -174,14 +173,13 @@ export default function icon(ms) {
         ms._iconCache[icnet].numberSIDC = {};
         ms._iconCache[icnet].numberSIDC.symbolSet = {};
         if (typeof ms._getIcons.number === "function") {
-          ms._iconCache[icnet].numberSIDC.symbolSet[
-            symbolSet
-          ] = ms._getIcons.number(
-            ms,
-            symbolSet,
-            iconParts,
-            this.metadata.STD2525
-          );
+          ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet] =
+            ms._getIcons.number(
+              ms,
+              symbolSet,
+              iconParts,
+              this.metadata.STD2525
+            );
           icons = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].icons;
           m1 = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].m1;
           m2 = ms._iconCache[icnet].numberSIDC.symbolSet[symbolSet].m2;
@@ -240,8 +238,7 @@ export default function icon(ms) {
         type: "path",
         stroke: false,
         fill: iconColor,
-        d:
-          "m 94.8206,78.1372 c -0.4542,6.8983 0.6532,14.323 5.3424,19.6985 4.509,5.6933 11.309,9.3573 14.98,15.7283 3.164,6.353 -0.09,14.245 -5.903,17.822 -7.268,4.817 -18.6219,2.785 -22.7328,-5.249 -1.5511,-2.796 -2.3828,-5.931 -2.8815,-9.071 -3.5048,0.416 -7.0093,0.835 -10.5142,1.252 0.8239,8.555 5.2263,17.287 13.2544,21.111 7.8232,3.736 17.1891,3.783 25.3291,1.052 8.846,-3.103 15.737,-11.958 15.171,-21.537 0.05,-6.951 -4.272,-12.85 -9.134,-17.403 -4.526,-4.6949 -11.048,-8.3862 -12.401,-15.2748 -1.215,-2.3639 -0.889,-8.129 -0.889,-8.129 z m -0.6253,-20.5177 0,11.6509 11.6527,0 0,-11.6509 z",
+        d: "m 94.8206,78.1372 c -0.4542,6.8983 0.6532,14.323 5.3424,19.6985 4.509,5.6933 11.309,9.3573 14.98,15.7283 3.164,6.353 -0.09,14.245 -5.903,17.822 -7.268,4.817 -18.6219,2.785 -22.7328,-5.249 -1.5511,-2.796 -2.3828,-5.931 -2.8815,-9.071 -3.5048,0.416 -7.0093,0.835 -10.5142,1.252 0.8239,8.555 5.2263,17.287 13.2544,21.111 7.8232,3.736 17.1891,3.783 25.3291,1.052 8.846,-3.103 15.737,-11.958 15.171,-21.537 0.05,-6.951 -4.272,-12.85 -9.134,-17.403 -4.526,-4.6949 -11.048,-8.3862 -12.401,-15.2748 -1.215,-2.3639 -0.889,-8.129 -0.889,-8.129 z m -0.6253,-20.5177 0,11.6509 11.6527,0 0,-11.6509 z",
       },
     ];
     if (this.metadata.numberSIDC) {
@@ -269,13 +266,42 @@ export default function icon(ms) {
         }
       } else {
         //Handle special cases of dismounted individual where weapons should be scaled
-        // TODO take care of common modifiers...
+        const symbolSet = String(this.options.sidc).substring(4, 6);
         const mainSIDC = Number(this.metadata.functionid.substr(0, 6));
         if (
-          (this.metadata.dismounted &&
+          /*this.metadata.dismounted*/ (symbolSet == "27" &&
             mainSIDC >= 110301 &&
             mainSIDC <= 110403) ||
-          this.metadata.landequipment
+          (symbolSet == "15" &&
+            // Symbols that shouldn't scale
+            [
+              "130100",
+              "170000",
+              "170400",
+              "170600",
+              "170700",
+              "170800",
+              "170900",
+              "171100",
+              "200200",
+              "200300",
+              "200600",
+              "200700",
+              "200800",
+              "200900",
+              "201100",
+              "201301",
+              "201302",
+              "201400",
+              "210100",
+              "210200",
+              "210300",
+              "210400",
+              "210500",
+              "230200",
+              "250000",
+            ].indexOf(mainSIDC) == -1)
+          //this.metadata.landequipment
         ) {
           if (
             this.metadata._modifier1 != "000" &&
