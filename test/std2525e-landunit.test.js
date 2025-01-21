be7f70f1f1b6f6d305d8ce47ba3fa4738531863b
Måns Beckman <mans.beckman@spatialillusions.com>
2025-01-21 15:50:11 +0100

    Add 2525E/APP6E icon tests

---
+++ test/std2525e-landunit.test.js
@@ -0,0 +1,9 @@
+import { ms } from "../src/milsymbol.js";
+ms.reset();
+import { ms2525e } from "milstandard-e";
+import verify from "./number-sidc.js";
+
+import { landunit as icons } from "../src/numbersidc.js";
+ms.addIcons(icons);
+
+export default verify(ms, ms2525e["10"]);
