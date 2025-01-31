5ceebcfda3993ad7e6b891ae31e259f8b09f9ead
Måns Beckman <mans.beckman@spatialillusions.com>
2025-01-31 13:47:56 +0100

    Delete std2525d-cyberspace.test.js

--- test/std2525d-cyberspace.test.js
+++
@@ -1,9 +0,0 @@
-import { ms } from "../src/milsymbol.js";
-ms.reset();
-import { ms2525e } from "milstandard-e";
-import verify from "./number-sidc.js";
-
-import { cyberspace as icons } from "../src/numbersidc.js";
-ms.addIcons(icons);
-
-export default verify(ms, ms2525e["60"]);
