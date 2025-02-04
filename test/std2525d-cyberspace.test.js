b4887755fa50054a84343668cdcc86a6ab4cfb73
Måns Beckman <mans.beckman@spatialillusions.com>
2025-02-04 18:12:49 +0100

    Create std2525d-cyberspace.test.js

---
+++ test/std2525d-cyberspace.test.js
@@ -0,0 +1,9 @@
+import { ms } from "../src/milsymbol.js";
+ms.reset();
+import { ms2525d } from "mil-std-2525";
+import verify from "./number-sidc.js";
+
+import { cyberspace as icons } from "../src/numbersidc.js";
+ms.addIcons(icons);
+
+export default verify(ms, ms2525d["60"]);
